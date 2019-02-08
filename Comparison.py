bc7e18441982536bcaf9ab58f239b8fd1f66353a
Soujanya <soujanyaburagapu@yahoo.com>
2019-02-08 12:42:27 -0500

    First Commit

---
+++ Comparison.py
@@ -0,0 +1,35 @@
+a = 21
+b = 10
+c = 0
+
+if ( a == b ):
+    print ("(21 = 10) -> a is equal to b")
+else:
+    print ("(21 = 10) -> a is not equal to b")
+
+if ( a != b ):
+    print ("a is not equal to b")
+else:
+    print ("a is equal to b")
+
+if ( a < b ):
+    print ("a is less than b")
+else:
+    print ("a is not less than b")
+
+if ( a > b ):
+    print ("a is greater than b")
+else:
+    print ("a is not greater than b")
+
+a = 5;
+b = 20;
+if ( a <= b ):
+    print ("a is either less than or equal to  b")
+else:
+    print ("a is neither less than nor equal to  b")
+
+if ( b >= a ):
+    print ("b is either greater than  or equal to b")
+else:
+    print ("b is neither greater than  nor equal to b")
